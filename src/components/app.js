1aef6c92f4457ac5b265cd9f657fbdf4566809dd
Dmgonzo97 <109998281+Dmgonzo97@users.noreply.github.com>
2022-11-16 01:10:35 -0500

    Added Portfolio container section, added placeholder code, fixed import on app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import moment from "moment"; 
+import moment from "moment";
+
+import PortfolioContainer from './portfolio/portfolio-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Deion Gonzalez Portfolio</h1>
         
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <h1>Deion Gonzalez Portfolio</h1>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
 
       </div>
     );
