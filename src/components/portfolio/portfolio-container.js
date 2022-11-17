9c6a9f79c39e5eaf079fd572c3d029a72f688313
Dmgonzo97 <109998281+Dmgonzo97@users.noreply.github.com>
2022-11-16 22:26:05 -0500

    Added Custom function using map(), added props, and utilized JSX

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,12 +3,26 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        console.log('Portfolio has been rendered');
+    }
+    
+    portfolioItems() {
+        const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"];
+
+        return data.map(item => {
+            return <PortfolioItem title={item} url="Google.com" />;
+        });
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here updated...</h2>
 
-                <PortfolioItem />
+                {this.portfolioItems()}
             </div>
         )
     };
