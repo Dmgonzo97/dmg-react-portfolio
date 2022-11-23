5c4a6202a2f76d2a99e38988f17f54389d673503
Dmgonzo97 <109998281+Dmgonzo97@users.noreply.github.com>
2022-11-22 21:33:45 -0500

    Added many imports and the router. created pages folder

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import PortfolioContainer from './portfolio/portfolio-container';
 
@@ -7,6 +8,16 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
+
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path=""></Route>
+            </Switch>
+          </div>
+        </Router>
         
         <h1>Deion Gonzalez Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,23 +6,50 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
-        console.log('Portfolio has been rendered');
+        this.state = {
+            pageTitle: "Welcome to my Portfolio",
+            isLoading: false,
+            data: [
+                {title: "Quip", catagory: "eCommerce" },
+                {title: "Eventbrite", catagory: "Scheduling" },
+                {title: "Ministry Safe", catagory: "Enterprise"  },
+                {title: "SwingAway", catagory: "eCommerce"  }
+            ]
+        };
+        this.handleFilter = this.handleFilter.bind(this);
     }
     
-    portfolioItems() {
-        const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"];
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.catagory === filter;
+            })
+        })
+    }
 
-        return data.map(item => {
-            return <PortfolioItem title={item} url="Google.com" />;
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url="Google.com" />;
         });
     }
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>
+        }
+
+        
+
         return (
             <div>
-                <h2>Portfolio items go here updated...</h2>
+                <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
+                <button onClick={() => this.handleFilter("Scheduling")}>Scheduling</button>
+                <button onClick={() => this.handleFilter("Enterprise")}>Enterprise</button>
 
                 {this.portfolioItems()}
+
             </div>
         )
     };
