23aba74300265c3c501cc255252aebe6527d330c
Dmgonzo97 <109998281+Dmgonzo97@users.noreply.github.com>
2022-11-22 22:12:14 -0500

    Navigation bar added, along with more routes and pages folder w/ about, blog, contact, and a home page

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,11 @@ import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from "./navigation/navigation-container"
+import Home from "./pages/home"
+import About from "./pages/about"
+import Contact from "./pages/contact"
+import Blog from "./pages/blog"
 
 export default class App extends Component {
   render() {
@@ -14,7 +19,11 @@ export default class App extends Component {
             <NavigationContainer />
 
             <Switch>
-              <Route exact path=""></Route>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/about-me" component={About}></Route>
+              <Route exact path="/contact" component={Contact}></Route>
+              <Route exact path="/blog" component={Blog}></Route>
+
             </Switch>
           </div>
         </Router>
